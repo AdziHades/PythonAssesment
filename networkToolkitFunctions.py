7be51f603f61501a81e75de1bebfae5186b6c085
Adam Jacobs <adamjacobs1994@live.co.uk>
2018-12-14 11:19:51 +0000

    Update networkToolkitFunctions.py

--- networkToolkitFunctions.py
+++ networkToolkitFunctions.py
@@ -5,11 +5,11 @@ import collections
 
 class networkToolkitFunctions:
 
+	#Global variables
 	dict_FileHashed = {["File Path" : "File Hash"]}
 	l_DuplicateFiles = []
 	msg_ErrorMessage = ""
 	
-
     #Created by Adam Jacobs. 12/12/2018
     #Takes a file path as a string and returns the hash value of the file.
     def hashFile(filePath):
@@ -31,7 +31,7 @@ class networkToolkitFunctions:
 			l_FileHashes.append(hashFile(par_CurrentFile))
 			dict_FileHashed[par_CurrentFile] = hashFile(par_CurrentFile)
 		except:
-			msg_ErrorMessage = "Unable to compare file path", par_CurrentFile);
+			msg_ErrorMessage = "Unable to compare file path " + par_CurrentFile
 		
 	#Created by Adam Jacobs. 14/12/2018
 	#Detect duplicates from the dict_FileHashed dictionary and add the duplicates to the l_DuplicateFiles list object.
@@ -45,3 +45,5 @@ class networkToolkitFunctions:
 		for fileName, fileHashValue in dict_FileHashed.items():
 			if fileValues.count(fileHashValue) > 1:
 				l_DuplicateFiles.append(fileName)
+				
+	
