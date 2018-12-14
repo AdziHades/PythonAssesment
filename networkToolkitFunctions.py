d12efce8cb667b13babbe1af64d3fe815e8c8368
Adam Jacobs <adamjacobs1994@live.co.uk>
2018-12-14 09:56:56 +0000

    Update networkToolkitFunctions.py
    Additional functions created:
         recordFileHash
         detectDuplicates
    
    Added objects for:
         dictionary 'dict_FileHashed'
         list ''l_DuplicateFiles'
         string variable 'msg_ErrorMessage'

--- networkToolkitFunctions.py
+++ networkToolkitFunctions.py
@@ -1,16 +1,15 @@
-#-------------------------------------------------------------------------------
-# Name:        Network Maintenance Toolkit.
-# Purpose:     Class file containing the functions used in the Network Maintenance Toolkit.
-# Author(s):   Adam Jacobs, Ben Walton & Will Brown.
-# Created:     11/12/2018.
-
-#-------------------------------------------------------------------------------
-
 #Organise imports
 import hashlib
 import easygui
+import collections
 
 class networkToolkitFunctions:
+
+	dict_FileHashed = {["File Path" : "File Hash"]}
+	l_DuplicateFiles = []
+	msg_ErrorMessage = ""
+	
+
     #Created by Adam Jacobs. 12/12/2018
     #Takes a file path as a string and returns the hash value of the file.
     def hashFile(filePath):
@@ -22,8 +21,27 @@ class networkToolkitFunctions:
 
         return hashLib.hexdigest()
 
-    #Create by **Add author information and date
-    #**Add function description
-    def compareFile(currentFile, compareList):
-        print("Throw new nonImplementedMethod exception")
-
+    #Created by Adam Jacobs. 14/12/2018
+	#Takes a file path as a parameter and adds the hash value to the global list l_FileHashes
+    def recordFileHash(par_CurrentFile):
+		global l_FileHashes
+		global msg_ErrorMessage
+	
+		try:
+			l_FileHashes.append(hashFile(par_CurrentFile))
+			dict_FileHashed[par_CurrentFile] = hashFile(par_CurrentFile)
+		except:
+			msg_ErrorMessage = "Unable to compare file path", par_CurrentFile);
+		
+	#Created by Adam Jacobs. 14/12/2018
+	#Detect duplicates from the dict_FileHashed dictionary and add the duplicates to the l_DuplicateFiles list object.
+	def detectDuplicates():
+		global l_DuplicateFiles
+		
+		#Get the values from the dictionary.
+		fileValues = dict_FileHashed.values()
+		
+		#Get the files from the dictionary where the file hash value exists more than once.
+		for fileName, fileHashValue in dict_FileHashed.items():
+			if fileValues.count(fileHashValue) > 1:
+				l_DuplicateFiles.append(fileName)
